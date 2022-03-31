471bc3b3615b990abbf6052a4c6390c3037b786e
Faisal Whelpley <fgwhelp@illinois.edu>
2022-03-31 17:30:25 -0500

    remove all types

--- typings.d.ts
+++ typings.d.ts
@@ -1,7 +1,1 @@
-declare module '@fgwhelpley/text-diff' {
-	export class diff {
-		main(left: string, right: string): any[];
-		cleanupSemantic(d: any): void;
-		prettyHtml(d: any): string;
-	}
-}
+declare module '@fgwhelpley/text-diff' 
