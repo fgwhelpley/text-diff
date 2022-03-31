67ee23232af510502c2caaa3fb3c6796f51febfd
Faisal Whelpley <fgwhelp@illinois.edu>
2022-03-31 17:09:45 -0500

    add some types

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,8 @@
-declare module '@fgwhelpley/text-diff' {}
+declare module '@fgwhelpley/text-diff' {
+	export interface Diffs {}
+	export class Diff {
+		main(left: string, right: string): Diffs[];
+		cleanupSemantic(d: Diffs): void;
+		prettyHtml(d: Diffs): string;
+	}
+}
